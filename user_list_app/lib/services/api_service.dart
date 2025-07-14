6e017c89ab685df9aa7ff4708cf63f1b4c200e3a
thisarakaushan <thisarak9992@gmail.com>
2025-07-14 22:18:15 +0530

    Updated scrolling and loading logics

--- user_list_app/lib/services/api_service.dart
+++ user_list_app/lib/services/api_service.dart
@@ -11,7 +11,9 @@ import '../utils/constants.dart';
 class ApiService {
   // Fetch users from the API
   /// [page] is the page number to fetch users from.
-  Future<List<User>> fetchUsers(int page) async {
+  // Future<List<User>> fetchUsers(int page) async {
+  // Get both users (list of User objects) and totalPages.
+  Future<Map<String, dynamic>> fetchUsers(int page) async {
     final response = await http.get(
       Uri.parse('$baseUrl/users?page=$page'),
       headers: headers,
@@ -22,8 +24,18 @@ class ApiService {
       final Map<String, dynamic> data = jsonDecode(response.body);
       // Extract the list of users from the response
       final List<dynamic> usersJson = data['data'];
+      // final List<dynamic> usersJson = data['data'] ?? [];
+
+      // Check if there are more pages
+      // final int totalPages = data['total_pages'] ?? 1;
+      final int totalPages = data['total_pages'] as int;
+
       // Convert the list of JSON objects to a list of User models
-      return usersJson.map((json) => User.fromJson(json)).toList();
+      // return usersJson.map((json) => User.fromJson(json)).toList();
+      final List<User> users = usersJson
+          .map((json) => User.fromJson(json))
+          .toList();
+      return {'users': users, 'total_pages': totalPages};
     } else {
       throw Exception('Failed to load users: ${response.statusCode}');
     }
